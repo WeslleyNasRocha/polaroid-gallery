93b19a9aeb9d4b0331a1bea4961a24519d7f09e7
Weslley Rocha <weslley.rocha@wildlifestudios.com>
2024-05-20 19:46:16 -0300

    refactor: using printMode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -28,23 +28,11 @@ export default function Component() {
     }
   };
 
-  useEffect(() => {
-    const print = async () => {
-      await wait(500);
-      window.print();
-      await wait(500);
-      setPrintMode(false);
-    };
-    if (printMode) {
-      print();
-    }
-  }, [printMode]);
-
   return (
     <main
       className={
         printMode
-          ? "m-[3mm] min-h-screen"
+          ? "min-w-screen min-h-screen"
           : "flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 p-4 md:p-8"
       }
     >
@@ -114,7 +102,24 @@ export default function Component() {
         </div>
       ) : (
         <>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="print:hidden flex justify-center my-4 gap-2">
+            <Button
+              className="flex items-center justify-center px-4 py-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-colors"
+              variant="outline"
+              onClick={() => setPrintMode(false)}
+            >
+              <DeleteIcon className="mr-2 h-5 w-5" />
+              Go Back
+            </Button>
+            <Button
+              className="inline-flex items-center justify-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md cursor-pointer transition-colors"
+              onClick={() => window.print()}
+            >
+              <PrinterIcon className="mr-2 h-5 w-5" />
+              Print
+            </Button>
+          </div>
+          <div className="grid grid-cols-4 gap-4">
             {photos.map((p, i) => (
               <div
                 key={i}
